a9562a7c92d397085e02895d2fcf957284f4fbc7
KUSKIS <sabinemaillet@gmail.com>
2020-11-13 13:51:25 +0100

    First method to public list of posts via ControllerHome

---
+++ controllers/ControllerHome.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once('views/View.php');
+require_once('models/PostManager.php');
+
+
+class ControllerHome
+{
+
+    private $PostManager;
+    private $view;
+
+    public function __construct() //retrait parametre $url
+    {
+        if (isset($url) && count($url) > 1) {
+            throw new Exception("Page introuvable", 1);
+        } else {
+            $this->listChapters();
+        }
+
+        $this->_chapterManager = new PostManager();
+    }
+
+    //afficher tous les chapitres
+    public function listChapters()
+    {
+        $this->postManager = new PostManager();
+        $chapters = $this->postManager->getListChapters();
+        $this->view = new View('Home');
+        $this->view->generate(array('chapters' => $chapters));
+    }
+
+
+}
---
+++ models/PostManager.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once('models/Post.php');
+require_once('models/Model.php');
+
+class PostManager extends Model
+{
+
+    public function getAll()
+    {
+        $db = $this->dbConnect();
+        $var = [];
+        $req = $db->query(' SELECT * FROM  chapters  ORDER BY id desc ');
+        $req->execute();
+        //on crée la variable data qui contient les données
+
+        while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
+            //var contiendra les donnees sous forme d'objet
+            $var[] = new Post($data);
+        }
+
+        return $var;
+        $req->closeCursor(); //on maintient la requete
+
+    }
+
+    public function getListChapters()
+    {
+        return $this->getAll('chapters', 'Chapter');
+    }
+
+
+
+
+}
