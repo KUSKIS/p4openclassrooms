3af45bb0dfc92fd55a18d114be27b3bfb11e7a26
KUSKIS <sabinemaillet@gmail.com>
2020-11-19 17:10:46 +0100

    Update User.php

--- models/User.php
+++ models/User.php
@@ -13,13 +13,10 @@ class User
         $this->hydrate($data);
     }
 
-    //hydratation
 
     public function hydrate(array $data)
     {
         foreach ($data as $key => $value) {
-            //on crée les setters du tableau recu: setId, setTitle
-            //pour définir les valeurs des attibuts
             $method = 'set' . ucfirst($key);
             if (method_exists($this, $method)) {
                 $this->$method($value);
